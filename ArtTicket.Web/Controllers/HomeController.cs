b7409ee2ca0f249a7d055e3f14fd67aa45ed8275
ilie.melnic <ilie.melnic@isa.utm.md>
2025-03-08 04:00:52 +0200

    контроллер

---
+++ ArtTicket.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtTicket.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Refund()
+        {
+            return View();
+        }
+
+        public ActionResult Complaints()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
